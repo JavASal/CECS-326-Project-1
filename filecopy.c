75bfc2167d31e367e7ce37c912d75c0862637427
JavASal <javier.salamanca01@student.csubl.edu>
2025-09-30 16:16:42 -0700

    Working prototype under linux.

--- filecopy.c
+++ filecopy.c
@@ -3,27 +3,49 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
+    // Check to see if correct number of arguments were passed in.
+    /*
+     * list of accepted arguments
+     * 1. program name
+     * 2. source file name, must be in same directory as program
+     * 3. destination file to write to, will be placed in same directory as program
+     */
+    if(argc != 3){
+        printf("Error: Incorrect number of arguments passed.\nRun command as follows:\n");
+        printf("\'./<program name> <source file> <destination file>\'\n");
+        exit(1);
+    }
+
+    // Check to see if the input file exists before proceeding
+    FILE *inputFileCheck = fopen(argv[1], "r");
+    if(inputFileCheck == NULL){
+        printf("Error: Unable to open source file \'%s\'.\n", argv[1]);
+        exit(1);
+    }
+
+    // Variables for pipe and child process
     int pipefd[2];
     pid_t pid;
 
     // Create the pipe, check for errors
     if (pipe(pipefd) == -1) {
-        printf("Error creating a pipe.\n");
-        exit(1);
+        printf("Error: Unable to create pipe.\n");
+        exit(2);
     }
 
+    //Error handling in case child creation fails
     pid = fork();
     if (pid == -1) {
-        printf("Error during fork.\n");
-        exit(1);
+        printf("Error: Unable to create child process.\n");
+        exit(3);
     }
 
     // Child process
     if (pid == 0) {
-        // Close unused pipe write end in child process
+        // Child process does not need to use the write portion of the pipe so we close it
         close(pipefd[1]);
 
-        // Check if the output file exists and warn the user
+        // Checks to see if the output file already exists, if so it show warning to user. Need to add y/n option.
         FILE *myFile = fopen(argv[2], "r");
         if (myFile != NULL) {
             printf("Output file exists and will be overwritten!\n");
@@ -40,23 +62,24 @@ int main(int argc, char *argv[]) {
             fputc(ch, myFile);
         }
 
-        // Close pipe and file after use
+        // Close pipe and file
         close(pipefd[0]);
         fclose(myFile);
+        printf("File successfully copied from %s to %s.\n", argv[1], argv[2]);
     }
     // Parent process
     else {
-        // Close unused pipe read end in parent process
+        // Parent process does not need to use the read portion of the pipe so we close it
         close(pipefd[0]);
 
         // Open the input file for reading
         FILE *input = fopen(argv[1], "r");
         if (input == NULL) {
-            printf("There was an error opening the file in the parent process.\n");
+            printf("Error: Unable to open the file in the parent process.\n");
             exit(1);
         }
 
-        // Read characters from the input file and write them to the pipe
+        // Read characters from the input file and write them to the pipe, need to look into how to use a proper buffer
         char ch;
         while ((ch = fgetc(input)) != EOF) {
             write(pipefd[1], &ch, 1);
